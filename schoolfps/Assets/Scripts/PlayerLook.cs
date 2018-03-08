462e29124a3e849cb5253eeea98086f9e0fdddaf
Howligan <redrebel62@gmail.com>
2018-03-08 05:18:56 -0500

    Created First Person Character
    Simple Terrain
    Person Created
    Can move with WASD and look with mouse

---
+++ schoolfps/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour {
+
+    public float mouseSensitivity;
+    private float xAxisClamp = 0.0f;
+    public Transform playerBody;
+
+    // Use this for initialization
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RotateCamera(); // Making a method outside of the update and using update to call it is much easier to work with.
+        Cursor.lockState = CursorLockMode.Locked; // This prevents the cursor from moving around. It is locked to the center of the screen.
+    }
+
+    void RotateCamera() // IMPORTANT NOTE: This code only works for mouse movement as of 3/8/18. Controller will be implemented later.
+    {
+        float mouseX = Input.GetAxis("Mouse X"); // This gets the mouse position from the mouse's x. It is like the default
+        float mouseY = Input.GetAxis("Mouse Y"); // This gets the mouse position from the mouse's y. It is like the default
+
+        float rotAmountX = mouseX * mouseSensitivity; //This multiplies the mouse's default X value by the sensitivity. This is so the player can change the sensitivity
+        float rotAmountY = mouseY * mouseSensitivity; //This multiplies the mouse's default y value by the sensitivity. This is also so the player can change the sensitivity
+
+        Vector3 targetRotCam = transform.rotation.eulerAngles; // targetRotCam is used to get the current rotation of the camera. The Vector 3 is converted into Euler.
+        Vector3 targetRotBody = playerBody.rotation.eulerAngles; // targetRotBody is used to get the rotation of the body. It is a vec3 converted into euler.
+
+        // It is X > Y, and Y > X, because we are rotating around the Y axis using the mouse X and vice versa.
+        targetRotCam.x -= rotAmountY; // VERTICAL looking (up and down). The -= is to invert the rotation.
+        targetRotCam.z = 0; // This is to stop the camera from flipping when you look too far down or up.
+        targetRotBody.y += rotAmountX; // Syncs the y camera(HORIZONTAL) rotation with the body's rotation.
+
+        xAxisClamp -= rotAmountY;
+
+        if (xAxisClamp > 90)
+        {
+            xAxisClamp = 90;
+            targetRotCam.x = 90;
+        }
+        else if (xAxisClamp < -90)
+        {
+            xAxisClamp = -90;
+            targetRotCam.x = 270;
+        }
+
+        transform.rotation = Quaternion.Euler(targetRotCam); // Converting the euler into a vector 3
+        playerBody.rotation = Quaternion.Euler(targetRotBody); // Converting the euler into vec3
+    }
+}
---
+++ schoolfps/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    private CharacterController charControl;
+    public float speed;
+   
+
+
+	// Use this for initialization
+	void Awake () {
+        charControl = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+         MovePlayer();
+        //myControls();
+	}
+
+    void MovePlayer()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        Vector3 moveDirSide = transform.right * hor * speed;
+        Vector3 moveDirForward = transform.forward * ver * speed;
+
+        charControl.SimpleMove(moveDirSide);
+        charControl.SimpleMove(moveDirForward);
+    }
+
+    void myControls()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            Debug.Log("a");
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            Debug.Log("d");
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            Debug.Log("w");
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+            Debug.Log("s");
+        }
+    }
+}
